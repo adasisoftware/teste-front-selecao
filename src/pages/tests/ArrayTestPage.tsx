1d88dbb62568c7f74ebb9b182e256e0aea45398d
Thiago Souto <112123936+thiagosoutodev@users.noreply.github.com>
2025-05-28 13:54:57 -0300

    refactor: modularize constant

--- src/pages/tests/ArrayTestPage.tsx
+++ src/pages/tests/ArrayTestPage.tsx
@@ -1,36 +1,15 @@
 import { Button } from "../../components/Button";
+import { names } from "../../constants/names";
 
 export function ArrayTestPage() {
-  const names = [
-    "Oliver",
-    "Emma",
-    "Liam",
-    "Ava",
-    "Noah",
-    "Sophia",
-    "Elijah",
-    "Isabella",
-    "James",
-    "Mia",
-    "Benjamin",
-    "Charlotte",
-    "Lucas",
-    "Amelia",
-    "Mason",
-    "Harper",
-    "Logan",
-    "Evelyn",
-    "Ethan",
-    "Abigail",
-  ];
-
+  const originalArray = [...names];
   return (
     <div className="flex justify-center items-center gap-24">
       <div>
         <h2 className="mb-2 ml-1 text-white">Array original:</h2>
         <div className="w-60 h-96 px-12 py-4 bg-white shadow-2xl rounded-2xl overflow-auto">
           <ol className="list-decimal">
-            {names.map((name, idx) => {
+            {originalArray.map((name, idx) => {
               return <li key={idx}>{name}</li>;
             })}
           </ol>
